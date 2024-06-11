0749a94f1bfc0917c15f2da442c31179f6cbf9db
WAAutoMaton <a547673135@gmail.com>
2024-06-11 16:51:42 +0800

    Update

--- kernels/yield-os/yield-os.c
+++ kernels/yield-os/yield-os.c
@@ -27,5 +27,5 @@ int main() {
   pcb[0].cp = kcontext((Area) { pcb[0].stack, &pcb[0] + 1 }, f, (void *)1L);
   pcb[1].cp = kcontext((Area) { pcb[1].stack, &pcb[1] + 1 }, f, (void *)2L);
   yield();
-  panic("Should not reach here!");
+  panic("Should not reach here!\n");
 }
--- tests/am-tests/src/tests/intr.c
+++ tests/am-tests/src/tests/intr.c
@@ -1,15 +1,19 @@
 #include <amtest.h>
+#include <stdio.h>
 
 Context *simple_trap(Event ev, Context *ctx) {
   switch(ev.event) {
     case EVENT_IRQ_TIMER:
-      putch('t'); break;
+      putch('t'); putch('\n'); break;
     case EVENT_IRQ_IODEV:
-      putch('d'); break;
+      putch('d'); putch('\n'); break;
     case EVENT_YIELD:
-      putch('y'); break;
+      putch('y'); putch('\n'); break;
+	case EVENT_ERROR:
+      panic("EVENT_ERROR handled!\n"); break;
     default:
-      panic("Unhandled event"); break;
+	  printf("%d\n", ev.event);
+      panic("Unhandled event\n"); break;
   }
   return ctx;
 }
@@ -20,7 +24,7 @@ void hello_intr() {
   io_read(AM_INPUT_CONFIG);
   iset(1);
   while (1) {
-    for (volatile int i = 0; i < 10000000; i++) ;
+    for (volatile int i = 0; i < 100000; i++) ;
     yield();
   }
 }
